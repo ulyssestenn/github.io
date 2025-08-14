31987eb1add6a74ec97c4f35597e90a2c5388f03
B. Hunt <106725064+ulyssestenn@users.noreply.github.com>
2025-08-14 14:06:30 -0500

    Set up Eleventy templating

--- assets/include.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('[data-include]').forEach(el => {
-    fetch(el.getAttribute('data-include'))
-      .then(response => response.text())
-      .then(html => {
-        el.innerHTML = html;
-      });
-  });
-});
