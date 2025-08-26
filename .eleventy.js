f2b1e96fc2bd7cec8a1d021ca16d30a2267a7a59
B. Hunt <106725064+ulyssestenn@users.noreply.github.com>
2025-08-26 00:53:29 -0500

    Copy shelfscan directory and decouple Android page from global styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("shelfscan");
   eleventyConfig.addPassthroughCopy({"*.png": "."});
   eleventyConfig.addPassthroughCopy({"*.mp4": "."});
   eleventyConfig.addPassthroughCopy({"*.gif": "."});
